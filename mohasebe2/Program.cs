a09d2d7c4a8113067a11a663a72d6c0906cd5792
Mohamad alavi <Mohamad alavi@DESKTOP-9QCQADM>
2023-06-21 09:10:21 -0700

    Add project files.

---
+++ mohasebe2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace mohasebe2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("adad aval ra vared konid");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("adad dovom ra vared konid");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            int a = (int)Math.Pow(x, 5);
+            int b = (int)Math.Pow(x, 4);
+            int c = a + 2 * b;
+            int k = (int)Math.Pow(y, 2);
+            k = k - 7;
+            Console.WriteLine($"javab soal = {c * k}");
+            Console.ReadKey();
+
+
+        }
+    }
+}
